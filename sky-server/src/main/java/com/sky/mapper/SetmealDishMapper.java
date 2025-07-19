2fa84874716b25982f4da62999546d660e0d902b
Wang Xi <895561646@qq.com>
2025-07-19 19:37:47 +0800

    套餐管理

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,6 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -14,4 +17,26 @@ public interface SetmealDishMapper {
      */
     //select setmeal_id from setmeal_dish where dish_id in (1,2,3,4)
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+
+    /**
+     * 批量保存套餐和菜品的关系
+     * @param setmealDishes
+     */
+    void insertBatch(List<SetmealDish> setmealDishes);
+
+    /**
+     * 删除套餐id 删除套餐菜品关联信息
+     * @param id
+     */
+    @Delete("delete from setmeal_dish where setmeal_id = #{id}")
+    void deleteBySetmealId(Long id);
+
+
+    /**
+     * 根据套餐id查询套餐和菜品的关联关系
+     * @param setmealId
+     * @return
+     */
+    @Select("select * from setmeal_dish where  setmeal_id = #{id}")
+    List<SetmealDish> getBySetmealId(Long setmealId);
 }
